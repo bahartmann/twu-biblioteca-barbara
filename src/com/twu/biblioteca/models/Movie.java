dc399836a1422daa2065a6f4c57a72128d947e4d
bahartmann <barbarahartma@gmail.com>
2015-03-04 18:50:39 -0300

    Movie class

---
+++ src/com/twu/biblioteca/models/Movie.java
@@ -0,0 +1,44 @@
+package com.twu.biblioteca.models;
+
+/**
+ * Created by bhartman on 3/4/15.
+ */
+public class Movie extends LibraryItem {
+    private int year;
+    private String director;
+    private double rating;
+
+    public int getYear() {
+        return year;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public double getRating() {
+        return rating;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setDirector(String author) {
+        this.director = author;
+    }
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public Movie(String title, String director, int year, double rating) {
+        setTitle(title);
+        setDirector(director);
+        setYear(year);
+        setRating(rating);
+        setCheckedOut(false);
+    }
+
+    public void checkout() {
+        String checkoutStatus = checkoutItem() ? "Thank you! Enjoy the movie" : "That movie is not available.";
+        System.out.println(checkoutStatus);
+    }
+
+}
---
+++ test/com/twu/biblioteca/models/MovieTest.java
@@ -0,0 +1,49 @@
+package com.twu.biblioteca.models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+
+    private Movie unavailableMovie;
+    private Movie availableMovie;
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        unavailableMovie = new Movie("The Good, the Bad and the Ugly", "Sergio Leone", 1966, 8.9);
+        unavailableMovie.setCheckedOut(true);
+        availableMovie = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, 8.8);
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void shouldCreateMovieWhenValidDataIsGiven() {
+        Movie newMovie = new Movie("Casablanca", "Michael Curtiz", 1942, 8);
+        assertNotNull(newMovie);
+    }
+
+    @Test
+    public void successfulCheckout() {
+        availableMovie.checkout();
+        assertEquals("Thank you! Enjoy the movie\n", outContent.toString());
+    }
+
+    @Test
+    public void unsuccessfulCheckout() {
+        unavailableMovie.checkout();
+        assertEquals("That movie is not available.\n", outContent.toString());
+    }
+
+}
